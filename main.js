3b65b7768cc04b047cd49f69a5f10920eb24c1a7
Nicholas Duadei <nicholasduadei14@gmail.com>
2022-04-18 01:31:19 +0100

    initial desktop nav

--- main.js
+++ main.js
@@ -13,3 +13,17 @@ for (title of titles) {
     });
 }
 
+function nav() {
+    navStatus = document.getElementById('nav-bar')
+    if (Array.from(navStatus.classList).includes('show-nav')) {
+        navStatus.classList.remove('show-nav');
+        document.getElementById('line1').classList.remove('line1');
+        document.getElementById('line2').classList.remove('line2')
+    }
+    else {
+        navStatus.classList +=' show-nav'
+        document.getElementById('line1').classList += ' line1'
+        document.getElementById('line2').classList += ' line2'
+    }
+}
+
