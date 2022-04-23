d00e53f33a363894af31e3ec37cf5b8cf0dbcb7f
Nicholas Duadei <nicholasduadei14@gmail.com>
2022-04-24 00:36:38 +0100

    incomplete animations

--- main.js
+++ main.js
@@ -27,3 +27,28 @@ function nav() {
     }
 }
 
+function shuffle() {
+    let card1 = document.getElementById('atm1');
+    let card2 = document.getElementById('atm2');
+    let card3 = document.getElementById('atm3');
+    let card4 = document.getElementById('atm4');
+    let ids = [card1.id, card2.id, card3.id, card4.id]
+    let container = document.getElementById('atm-container');
+
+    // setInterval(() => {
+    //     card1.style.transform= 'scale(.8)';
+    //     card2.style.transform= 'scale(.8)';
+    //     card3.style.transform= 'scale(.8)';
+    //     card4.style.transform= 'scale(.8)';
+    // },2000)
+    // setInterval(() => {card2.style.transform= 'scale(1)'}, 700)
+    card1.id = ids[3]
+    card2.id= ids[0]
+    card3.id = ids[1]
+    card4.id = ids[2]
+    // container.style.transform= 'translateX(10px)'
+    
+}
+
+setInterval(shuffle, 2500)
+
